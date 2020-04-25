3d96e8804d8eae116c933d0942f417034799b938
Jonathan Morag <moragjonathan@gmail.com>
2020-04-25 13:06:05 +0300

    More style changes

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(favicon(__dirname + "/public/images/favicon.ico"));
+app.use(favicon(__dirname + "/public/images/news.png"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
